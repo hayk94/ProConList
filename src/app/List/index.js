070d2615df2012b00b6f03e035fc9be5b9e90161
Hayk <hayk94@gmail.com>
2018-04-07 17:44:22 +0400

    WIP ProConList

---
+++ src/app/List/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ListSubheader from 'material-ui/List/ListSubheader'
+import List, { ListItem, ListItemText } from 'material-ui/List'
+
+const ProConList = props => <List
+  subheader={<ListSubheader component='div'>Nested List Items</ListSubheader>}>
+
+  </List>
+
+export default ProConList
